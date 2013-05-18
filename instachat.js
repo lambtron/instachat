3217a4513b1171060def596bdbd5b952bdee106a
Andy Jiang <andyjiang@gmail.com>
2013-05-18 16:57:33 -0700

    added text box

--- instachat.js
+++ instachat.js
@@ -3,11 +3,13 @@ if (Meteor.isClient) {
     return "Welcome to instachat.";
   };
 
+  Template.stream.text = function() {
+    $('.chat').val();
+  };
+
   Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+    'keydown' : function() {
+      console.log($('.chat').val());
     }
   });
 }
