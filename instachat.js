01e1b7246ef4d7f6d6e87d6799148193caea66cc
Andy Jiang <andyjiang@gmail.com>
2013-05-18 17:56:54 -0700

    hard coded andy and mike

--- instachat.js
+++ instachat.js
@@ -1,15 +1,33 @@
+Texts = new Meteor.Collection("text");
+
 if (Meteor.isClient) {
+  // Add a new record in mongo.
+  // Texts.remove({});
+  var idAndy = Texts.insert({text: ''});
+  var idMike = Texts.insert({text: ''});
+  Session.set("current_user", idAndy);
+
   Template.hello.greeting = function () {
     return "Welcome to instachat.";
   };
 
-  Template.stream.text = function() {
-    $('.chat').val();
+  Template.streamAndy.text = function() {
+    // console.log(Texts.find(Session.get("current_user")).fetch()[0]['text']);
+    return Texts.find(idAndy).fetch()[0]['text'];
+  };
+  Template.streamMike.text = function() {
+    // console.log(Texts.find(Session.get("current_user")).fetch()[0]['text']);
+    return Texts.find(idMike).fetch()[0]['text'];
   };
 
   Template.hello.events({
-    'keydown' : function() {
-      console.log($('.chat').val());
+    'keyup input#andy' : function() {
+      var text = $('#andy').val();
+      Texts.update(idAndy, {text: text});
+    },
+    'keyup input#mike' : function() {
+      var text = $('#mike').val();
+      Texts.update(idMike, {text: text});
     }
   });
 }
@@ -17,5 +35,6 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    Texts.remove({});
   });
 }
