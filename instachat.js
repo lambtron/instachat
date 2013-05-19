ec8dd5669e91ab1ab3c4730eed632feb7fa82aef
Michael Wei <subwei@Michaels-MacBook-Pro.local>
2013-05-18 18:55:22 -0700

    working prototype

--- instachat.js
+++ instachat.js
@@ -1,11 +1,19 @@
+// Set up a collection to contain player information. On the server,
+// it is backed by a MongoDB collection named "players".
+
+Messages = new Meteor.Collection("messages");
+Players = new Meteor.Collection("players");
+
 Texts = new Meteor.Collection("text");
+
 
 if (Meteor.isClient) {
   // Add a new record in mongo.
   // Texts.remove({});
-  var idAndy = Texts.insert({text: ''});
-  var idMike = Texts.insert({text: ''});
-  Session.set("current_user", idAndy);
+  var idAndy = '1';
+  var idMike = '2';
+  Texts.insert({ _id: idAndy, text: ''});
+  Texts.insert({ _id: idMike, text: ''});
 
   Template.hello.greeting = function () {
     return "Welcome to instachat.";
@@ -30,11 +38,61 @@ if (Meteor.isClient) {
       Texts.update(idMike, {text: text});
     }
   });
+
+  // Template.chat1.events({
+  //   'keyup input.area' : function () {
+  //       Players.update(Session.get("selected_player"), {$set: {"a": "hi"}});
+  //   }
+  // });
+
+  // Template.display1.text = function () {
+  //   return Messages.findOne({});
+  // }
+
+
+  Template.leaderboard.players = function () {
+    return Players.find({}, {sort: {score: -1, name: 1}});
+  };
+
+  Template.leaderboard.selected_name = function () {
+    var player = Players.findOne(Session.get("selected_player"));
+    return player && player.name;
+  };
+
+  Template.player.selected = function () {
+    return Session.equals("selected_player", this._id) ? "selected" : '';
+  };
+
+  Template.leaderboard.events({
+    'click input.inc': function () {
+      Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+    }
+  });
+
+  Template.player.events({
+    'click': function () {
+      Session.set("selected_player", this._id);
+      Session.set("current_user", this._id)
+
+    }
+  });
 }
 
+function deleteDatabase() {
+  Players.remove({});
+  Messages.remove({});
+  Texts.remove({});
+};
+
+// On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
-    Texts.remove({});
+    deleteDatabase();
+    if (Players.find().count() === 0) {
+      var names = ["Ada Lovelace",
+                   "Claude Shannon"];
+      for (var i = 0; i < names.length; i++)
+        Players.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
+    }
   });
 }
