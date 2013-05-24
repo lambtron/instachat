9072b9fdeda2f1658d333e3b798d14201b9ca7f6
Andy Jiang <andyjiang@gmail.com>
2013-05-24 13:59:33 -0700

    testing

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var express = require('express')
   , io = require('socket.io').listen(server)
   , Hashids = require('hashids');
 
-// server.listen(8080);
 var port = process.env.PORT || 3000;
 server.listen(port, function() {
   console.log("Listening on " + port);
@@ -58,40 +57,74 @@ app.get('/:hash', function(req, res) {
 // usernames which are currently connected to the chat
 var usernames = {};
 
-io.configure(function() {
-  io.set("transports", ["xhr-polling"]);
-  io.set("polling duration", 10);
-});
+// io.configure(function() {
+//   io.set("transports", ["xhr-polling"]);
+//   io.set("polling duration", 10);
+// });
 
-io.sockets.on('connection', function (socket) {
+var chat = io
+  .of('/test')
+  .on('connection', function (socket) {
+    // when the client emits 'sendchat', this listens and executes
+    socket.on('sendchat', function (data) {
+      // we tell the client to execute 'updatechat' with 2 parameters
+      io.sockets.emit('updatechat', socket.username, data);
+    });
 
-  // when the client emits 'sendchat', this listens and executes
-  socket.on('sendchat', function (data) {
-    // we tell the client to execute 'updatechat' with 2 parameters
-    io.sockets.emit('updatechat', socket.username, data);
+    // when the client emits 'adduser', this listens and executes
+    socket.on('adduser', function(username){
+      // we store the username in the socket session for this client
+      socket.username = username;
+      // add the client's username to the global list
+      usernames[username] = username;
+      // echo to client they've connected
+      socket.emit('updatechat', 'SERVER', 'you have connected');
+      // echo globally (all clients) that a person has connected
+      socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+      // update the list of users in chat, client-side
+      io.sockets.emit('updateusers', usernames);
+    });
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', function(){
+      // remove the username from global usernames list
+      delete usernames[socket.username];
+      // update list of users in chat, client-side
+      io.sockets.emit('updateusers', usernames);
+      // echo globally that this client has left
+      socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+    });
   });
 
-  // when the client emits 'adduser', this listens and executes
-  socket.on('adduser', function(username){
-    // we store the username in the socket session for this client
-    socket.username = username;
-    // add the client's username to the global list
-    usernames[username] = username;
-    // echo to client they've connected
-    socket.emit('updatechat', 'SERVER', 'you have connected');
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
-    // update the list of users in chat, client-side
-    io.sockets.emit('updateusers', usernames);
-  });
+// io.sockets.on('connection', function (socket) {
 
-  // when the user disconnects.. perform this
-  socket.on('disconnect', function(){
-    // remove the username from global usernames list
-    delete usernames[socket.username];
-    // update list of users in chat, client-side
-    io.sockets.emit('updateusers', usernames);
-    // echo globally that this client has left
-    socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-  });
-});
+//   // when the client emits 'sendchat', this listens and executes
+//   socket.on('sendchat', function (data) {
+//     // we tell the client to execute 'updatechat' with 2 parameters
+//     io.sockets.emit('updatechat', socket.username, data);
+//   });
+
+//   // when the client emits 'adduser', this listens and executes
+//   socket.on('adduser', function(username){
+//     // we store the username in the socket session for this client
+//     socket.username = username;
+//     // add the client's username to the global list
+//     usernames[username] = username;
+//     // echo to client they've connected
+//     socket.emit('updatechat', 'SERVER', 'you have connected');
+//     // echo globally (all clients) that a person has connected
+//     socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+//     // update the list of users in chat, client-side
+//     io.sockets.emit('updateusers', usernames);
+//   });
+
+//   // when the user disconnects.. perform this
+//   socket.on('disconnect', function(){
+//     // remove the username from global usernames list
+//     delete usernames[socket.username];
+//     // update list of users in chat, client-side
+//     io.sockets.emit('updateusers', usernames);
+//     // echo globally that this client has left
+//     socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+//   });
+// });
